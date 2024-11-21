b3702fa029cf336638539ef6cc5bc20f4e450397
Locoyoccim <charlyhernandez917@gmail.com>
2024-11-20 20:06:47 -0600

    procfile new

--- UsersControl/views.py
+++ UsersControl/views.py
@@ -91,9 +91,13 @@ def login_user(request):
         return JsonResponse({"success": False, "message": "Email is required"}, status=400)
     
     user = User.objects.filter(email=email).first()
-
+    
     if user is None:
         return JsonResponse({"success": False, "message": "User not found"}, status=404)
-    
-   
+    elif user.blocked:
+        return JsonResponse("This user are blocked, call admin", safe=False, status=401)
+
+    user.last_seen = datetime.now()
+    user.save()
+
     return JsonResponse({'message': 'Login exitoso'}, status=200)
